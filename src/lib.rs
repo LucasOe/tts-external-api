ce0b54edc721b1ca59e460846d6c43ce01221e42
LucasOe <github@lucasoe.com>
2023-01-08 23:16:45 +0100

    Add tests

--- src/lib.rs
+++ src/lib.rs
@@ -500,26 +500,53 @@ impl ExternalEditorApi {
 
 #[cfg(test)]
 mod tests {
-    use crate::ExternalEditorApi;
-
-    #[test]
-    fn test_execute() {
-        let api = ExternalEditorApi::new();
-        let answer = api.execute(String::from("print('Test')\nreturn JSON.encode('5')"));
-        println!("{:?}", answer);
-    }
+    use crate::{AnswerNewObject, ExternalEditorApi};
+    use serde_json::{json, Value};
 
     #[test]
     fn test_get_scripts() {
         let api = ExternalEditorApi::new();
-        let answer = api.get_scripts();
-        let script_states = answer.script_states();
+
+        let script_states: Value = api.get_scripts().script_states();
         println!("{:#?}", script_states);
+    }
+
+    #[test]
+    fn test_reload() {
+        let api = ExternalEditorApi::new();
+
+        let script_states = api.get_scripts().script_states();
+        let script_states = api.reload(script_states).script_states();
+        println!("{:#?}", script_states);
+    }
+
+    #[test]
+    fn test_custom_message() {
+        let api = ExternalEditorApi::new();
+
+        api.custom_message(json![{"foo": "Foo", "bar": "Bar"}]);
+    }
+
+    #[test]
+    fn test_execute() {
+        let api = ExternalEditorApi::new();
+
+        let answer = api.execute(String::from("return JSON.encode('5')"));
+        println!("{:#?}", answer);
+    }
+
+    #[test]
+    fn test_new_object() {
+        let api = ExternalEditorApi::new();
+
+        let answer: AnswerNewObject = api.wait();
+        println!("{:#?}", answer);
     }
 
     #[test]
     fn test_read() {
         let api = ExternalEditorApi::new();
+
         loop {
             let answer = api.read();
             println!("{:#?}", answer);
